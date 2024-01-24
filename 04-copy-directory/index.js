5da34408556275c24fb5a610daf6c22139566c9d
Roman Kalinin <pancu43@gmail.com>
2024-01-24 20:58:40 +0500

    feat: change 4 task Wed, Jan 24, 2024 08:58:40 PM

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const fsPromises = require('fs/promises');
-
 const filesFolder = path.join(__dirname, './files');
 const copyFolder = path.join(__dirname, './files-copy');
 
@@ -10,8 +8,37 @@ fs.mkdir(copyFolder, { recursive: true }, (error) => {
   if (error) {
     return console.error(error);
   }
-
-  fsPromises.cp(filesFolder, copyFolder, { recursive: true });
-
-  console.log('\nФайлы успешно скопированны!\n');
 });
+
+fs.readdir(copyFolder, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    return console.log(error);
+  } else {
+    for (let i = 0; i < files.length; i++) {
+      const checkDeletedFiles = path.join(copyFolder, files[i].name);
+      fs.unlink(checkDeletedFiles, (error) => {
+        if (error) {
+          return console.log(error);
+        }
+      });
+    }
+  }
+});
+
+fs.readdir(filesFolder, { withFileTypes: true }, (error, files) => {
+  if (error) {
+    return console.log(error);
+  } else {
+    for (let i = 0; i < files.length; i++) {
+      const fileFromOriginFolder = path.join(filesFolder, files[i].name);
+      const fileFromCoppyFolder = path.join(copyFolder, files[i].name);
+      fs.copyFile(fileFromOriginFolder, fileFromCoppyFolder, (error) => {
+        if (error) {
+          return console.log(error);
+        }
+      });
+    }
+  }
+});
+
+console.log('Directory created successfully!');
